56cd4083b7934027424e3538950d02e3506229b3
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2019-12-24 08:38:52 +0600

    Initial Commit

---
+++ TabController-59462981/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabController-59462981
+//
+//  Created by Mufakkharul Islam Nayem on 12/24/19.
+//  Copyright © 2019 Mufakkharul Islam Nayem. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
